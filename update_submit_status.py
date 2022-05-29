a0965cc82e4e4d6893960cb7aac7263275c9ddb0
오채환 <coghks56@gmail.com>
2022-05-29 23:48:46 +0900

    리뷰 대상자 배정 로직 수정 및 검증

--- update_submit_status.py
+++ update_submit_status.py
@@ -40,32 +40,44 @@ data.loc[(data['slackID'].isin(pass_member_ids)) & (data['제출기한'] == due_
 data.loc[(~data['slackID'].isin(pass_member_ids)) & (data['제출기한'] == due_date), '패스사용여부'] = 'N'
 
 # 피드백대상자 배정
-reviewers = data.loc[(data['패스사용여부'] != 'Y') & (data['제출기한'] == due_date), 'slackID'].tolist()
-assign_count_map = {member_id: 0 for member_id in submit_member_ids}
-assign_count_map = Counter(assign_count_map)
-
 feedback_start_date = '2022-06-15'
 if datetime.datetime.strptime(feedback_start_date, '%Y-%m-%d') <= datetime.datetime.strptime(due_date, '%Y-%m-%d'):
+    reviewers = data.loc[(data['패스사용여부'] != 'Y') & (data['제출기한'] == due_date), 'slackID'].tolist()
+    assign_counter = {member_id: 0 for member_id in submit_member_ids}
+    assign_counter = Counter(assign_counter)
+
     review_assignments = []
     for reviewer in reviewers:
-        assign_counts = assign_count_map.most_common()
-        reviewees = [assign_count[0] for assign_count in assign_counts if assign_count[0] != reviewer]
-        first_reviewee = reviewees[-1]
-        second_reviewee = reviewees[-2]
-        assign_count_map.update({first_reviewee: 1})
-        assign_count_map.update({second_reviewee: 1})
+        assign_count_by_reviewee = assign_counter.most_common()
+        reviewees = [reviewee for reviewee, assign_count in assign_count_by_reviewee if reviewee != reviewer]
+        lowest_assign_count_member = reviewees[-1]
+        second_lowest_assign_count_member = reviewees[-2]
+        assign_counter.update({lowest_assign_count_member: 1})
+        assign_counter.update({second_lowest_assign_count_member: 1})
         review_assignments.append({
             "slackID": reviewer,
-            "피드백대상자1": slack_client.get_channel_member_name(member_id=first_reviewee),
-            "피드백대상자2": slack_client.get_channel_member_name(member_id=second_reviewee),
+            "피드백대상자1": slack_client.get_channel_member_name(member_id=lowest_assign_count_member),
+            "피드백대상자2": slack_client.get_channel_member_name(member_id=second_lowest_assign_count_member),
         })
-    review_assignments_data = pd.DataFrame(review_assignments)
-    review_assignments_data = review_assignments_data.set_index('slackID')
+    review_assignments_data = pd.DataFrame(review_assignments).set_index('slackID')
 
     data.loc[data['제출기한'] == due_date, '피드백대상자1'] = \
         data['slackID'].map(review_assignments_data['피드백대상자1']).fillna('-')
     data.loc[data['제출기한'] == due_date, '피드백대상자2'] = \
         data['slackID'].map(review_assignments_data['피드백대상자2']).fillna('-')
+
+    # 멤버별 리뷰 수 및 리뷰 못 받는 멤버 유무 체크
+    review_count_by_reviewee1 = data[data['제출기한'] == due_date].groupby('피드백대상자1')['성명'].count() \
+        .reset_index().rename(columns={'피드백대상자1': '피드백대상자'})
+    review_count_by_reviewee2 = data[data['제출기한'] == due_date].groupby('피드백대상자2')['성명'].count() \
+        .reset_index().rename(columns={'피드백대상자2': '피드백대상자'})
+    review_count_by_reviewee = pd.concat([review_count_by_reviewee1, review_count_by_reviewee2])
+    review_count_by_reviewee = review_count_by_reviewee.groupby('피드백대상자', as_index=False)['성명'].sum() \
+        .rename(columns={'성명': '인원수'})
+    print(review_count_by_reviewee)
+    submit_members = set([slack_client.get_channel_member_name(member_id) for member_id in submit_member_ids])
+    reviewees = set(review_count_by_reviewee['피드백대상자'].tolist())
+    print('리뷰 못 받는 멤버 유무 체크', submit_members - reviewees)
 
 worksheet.update([raw_data_columns] + data.values.tolist())
 print('complete!')
