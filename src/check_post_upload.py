653abfe4d41ab0c980c5f47b9d487da4b5d2f849
lumiamitie <lumiamitie@gmail.com>
2023-04-03 03:11:55 +0900

    add: 작업중 내용 업데이트(contentful, slack)

---
+++ src/check_post_upload.py
@@ -0,0 +1,117 @@
+import os
+import requests
+from dotenv import load_dotenv
+from pathlib import Path
+from datetime import datetime, timezone, timedelta
+
+from pprint import pprint
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+API_BASE_URL = 'https://api.contentful.com'
+CMA_TOKEN = os.getenv('CONTENTFUL_CMA_TOKEN')
+SPACE_ID = os.getenv('CONTENTFUL_SPACE_ID')
+ENVIRONMENT_ID = os.getenv('CONTENTFUL_ENVIRONMENT_ID')
+
+sample_articles = [
+    {'title': '북극성 지표, 어떻게(How)?', 'slug': 'about-north-star-metric', 'author_id': '371RhJKtAnp5aA8kdaQMNH', 'private': False, 'published_at': '2023-04-01T08:10:39.483Z', 'date': '2023-04-04T00:00+09:00'}, 
+    {'title': 'MACD 방법으로 시계열 데이터에서 변곡점 찾아보기', 'slug': 'find-inflection-point-with-macd', 'author_id': '7yk4llUZalWtEpmtwzDp5k', 'private': False, 'published_at': '2023-04-01T04:41:15.978Z', 'date': '2023-04-01T00:00+09:00'}, 
+    {'title': '경쟁사의 Log를 직접 볼 수 있다면? - 블록체인 Dune', 'slug': 'log-dune-wip', 'author_id': '4F228o8FrZYEvwT8U9ZD3h', 'private': False, 'published_at': '2023-03-26T11:10:17.769Z', 'date': '2023-04-02T00:00+09:00'}, 
+    {'title': '얕은 분석과 깊은 분석의 차이점', 'slug': 'What-is-deep-analysis', 'author_id': '5WRi9m1YguzzfMxSQT651e', 'private': False, 'published_at': '2023-03-25T05:43:12.189Z', 'date': '2023-03-25T00:00+09:00'}
+]
+
+def get_contentful_articles() -> list:
+    """
+    Ouput: [{
+        "title": str,
+        "slug": str,
+        "author_id": str,
+        "private: bool,
+        "publisted_at": str,
+        "date": str
+    }]
+    """
+    req_url = API_BASE_URL + f'/spaces/{SPACE_ID}/environments/{ENVIRONMENT_ID}/public/entries?content_type=article'
+    response = requests.get(req_url, headers={'Authorization': f'Bearer {CMA_TOKEN}'})
+    if response.status_code == 200:
+        result = response.json()
+        articles = [{
+            'title': article['fields']['title']['en-US'],
+            'slug': article['fields']['slug']['en-US'],
+            'author_id': article['fields']['author']['en-US']['sys']['id'],
+            'private': article['fields']['private']['en-US'],
+            'published_at': article['sys']['publishedAt'],
+            'date': article['fields']['date']['en-US']
+        } for article in result['items']]
+        public_articles = list(filter(lambda a: a['private'] == False, articles))
+        return public_articles
+    else:
+        # TODO: 요청 실패시 예외처리
+        return []
+
+sample_authors = [
+    {'name': '박준하', 'slug': 'goldlant', 'author-id': '371RhJKtAnp5aA8kdaQMNH'}, 
+    {'name': '주정민', 'slug': 'jmin', 'author-id': '5rHNDnoqnwkIJyQSEikADA'}, 
+    {'name': '오연주', 'slug': 'yeonjuoh', 'author-id': '7yk4llUZalWtEpmtwzDp5k'}, 
+    {'name': '방태모', 'slug': 'taemobang', 'author-id': '4VK1xMT4iw9ustp6D8AW5P'}, 
+    {'name': '권남택', 'slug': 'namtaek', 'author-id': '6gHoqLPPbdiyxIQ0MTZNa8'}, 
+    {'name': '김민겸', 'slug': 'minkim', 'author-id': '4F228o8FrZYEvwT8U9ZD3h'}, 
+    {'name': '누리', 'slug': 'nuli', 'author-id': '58lKCEp8UM5xTzRrugtjm1'}, 
+    {'name': '이웅원', 'slug': None, 'author-id': '5WRi9m1YguzzfMxSQT651e'}, 
+    {'name': '이창현', 'slug': 'changhyun.lee', 'author-id': '30ylK3Wl9yg2YdyNlM9L9S'}, 
+    {'name': '홍선아', 'slug': 'soosunnaa', 'author-id': 'er9x59t9LsxaOcwxhjtft'}, 
+    {'name': '이인영', 'slug': 'nathan', 'author-id': '21xsO5PvrYaKjyLIa4DfMI'}, 
+    {'name': '윤정환', 'slug': 'alfred', 'author-id': '5UQFyzl5YwK852RxeOKwdF'}, 
+    {'name': '정원혁', 'slug': 'marvin', 'author-id': '6iHzmbpCFuZsWrjd4WVjq0'}, 
+    {'name': '이민호', 'slug': 'miika', 'author-id': '62y55ciisRKFSHp83U3G6u'}
+]
+
+def get_contentful_authors() -> list:
+    """
+    Ouput: [{
+        "name": str,
+        "slug": str,
+        "author_id": str
+    }]
+    """
+    req_url = API_BASE_URL + f'/spaces/{SPACE_ID}/environments/{ENVIRONMENT_ID}/public/entries?content_type=author'
+    response = requests.get(req_url, headers={'Authorization': f'Bearer {CMA_TOKEN}'})
+    if response.status_code == 200:
+        result = response.json()
+        authors = [{
+            'name': author['fields']['name']['en-US'],
+            'slug': author['fields'].get('slug', {}).get('en-US', None),
+            'author_id': author['sys']['id']
+        } for author in result['items']]
+        return authors
+    else:
+        # TODO: 요청 실패시 예외처리
+        return []
+
+
+def filter_articles_by_duedate(articles: list, due_start_date: str, due_end_date:str) -> list:
+    """
+    - CMS에서 퍼블리시한 날짜와 포스팅에 표기되는 게시글 날짜 모두가 due_start_date <= target <= due_end_date 에 포함되어야 한다.
+    - 정책으로 due_start_date 는 이전 배포일+1 이 되는 것이 좋다. (첫배포시에는 배포 2주 전쯤)
+    """
+    start_dt = datetime.strptime(due_start_date, '%Y-%m-%d')
+    end_dt = datetime.strptime(due_end_date, '%Y-%m-%d')
+    
+    # Set Timezone (Naive -> Aware)
+    kst_tz = timezone(timedelta(hours=9), 'KST')
+    start_dt = start_dt.replace(tzinfo=kst_tz)
+    end_dt = end_dt.replace(tzinfo=kst_tz)
+
+    result = []
+    for article in articles:
+        # published_dt: 2023-03-25T05:43:12.189Z -> 2023-03-25 14:43:12.189000+09:00
+        published_dt = datetime.strptime(article['published_at'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=kst_tz) + timedelta(hours=9)
+        article_dt = datetime.strptime(article['date'], '%Y-%m-%dT%H:%M%z')
+        if start_dt <= published_dt <= end_dt and start_dt <= article_dt <= end_dt:
+            result.append(article)
+    return result
+
+test = get_contentful_articles()
+test = filter_articles_by_duedate(test, '2023-03-20', '2023-04-03')
+pprint(test)
---
+++ src/check_slack_messages.py
@@ -0,0 +1,90 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from typing import List, Dict
+
+from pprint import pprint
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+# SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+
+def get_slack_client(token:str) -> WebClient:
+    return WebClient(token=token)
+
+def get_channel_members(client: WebClient, channel_id: str) -> list:
+    # #season3-team-publishers: C04SJDDSYHG
+    return client.conversations_members(channel=channel_id)['members']
+
+def get_member_name(client:WebClient, member_id:str) -> Dict[str, str]:
+    user_info = client.users_info(user=member_id)
+    
+    display_name = user_info['user']['profile']['display_name']
+    real_name = user_info['user']['real_name']
+    name = display_name if display_name != '' else real_name
+    
+    return {
+        'member_id': member_id,
+        'name': name
+    }
+
+client = get_slack_client(SLACK_BOT_TOKEN)
+# member_ids = get_channel_members(client, 'C04SJDDSYHG')
+# print([get_member_name(client, x) for x in member_ids])
+
+sample_members = [
+    {"member_id": "U0397UJ27FG", "name": "주정민"}, 
+    {"member_id": "U0397UJ7MK8", "name": "정원혁"}, 
+    {"member_id": "U039BMT9DU5", "name": "오연주"}, 
+    {"member_id": "U039EJ6FUJW", "name": "이강훈"}, 
+    {"member_id": "U039T6YQW6M", "name": "박종익"}, 
+    {"member_id": "U03S0PYHP32", "name": "홍선아"}, 
+    {"member_id": "U04K82RFBAS", "name": "김소정"}, 
+    {"member_id": "U04SCG3PQNB", "name": "진형민"}, 
+    {"member_id": "U04SP9YMVC6", "name": "이웅원"}, 
+    {"member_id": "U04SS8PFL0N", "name": "방태모"}
+]
+
+# Private 채널은 Group:read 권한이 필요한데, 현재 봇에 해당 권한이 없음
+def get_message_from_conversation(client: WebClient, channel_id:str) -> dict:
+    api_response = client.conversations_history(channel=channel_id, limit=3)
+    return api_response.data['messages']
+
+# https://playinpap.slack.com/archives/C04SJDDSYHG/p1680324093763889
+# p1680324093763889 여기서 앞에 p 제거후 뒤쪽 숫자 6자리 앞에 . 을 붙이면 ts값이 되는 듯
+# p1680324093763889 => 1680324093.763889
+# pprint(get_message_from_conversation(client, 'C04SJDDSYHG'))
+
+def get_replies_from_ts(client: WebClient, channel_id:str, ts: str) -> dict:
+    """
+    - 특정 스레드에서 답장을 누가 달았는지, 어떤 이모지가 달려있는지 체크
+    - ts와 thread_ts가 같을 경우 스레드 본문이기 때문에 제거
+    - 댓글 작성자가 직접 붙인 이모지에만 반응
+    
+    output:
+        [{'reactions': [], 'user': 'U04T7SZ0U1J'},
+        {'reactions': [], 'user': 'U04T7SZ0U1J'},
+        {'reactions': ['melting_face'], 'user': 'U02C4SP56H2'},
+        {'reactions': [], 'user': 'U04T7SZ0U1J'}]
+    """
+    api_response = client.conversations_replies(channel=channel_id, ts=ts)
+    if api_response.get('subtype') == 'bot_message':
+        return []
+    replies = list(filter(lambda m: m['ts'] != m['thread_ts'], api_response['messages']))
+    
+    def extract_reactions_from_reply(reply: dict) -> list:
+        reactions = reply.get('reactions', [])
+        reactions_from_replier = list(filter(lambda r: reply['user'] in r['users'], reactions))
+        emojis_from_replier = [reply['name'] for reply in reactions_from_replier]
+        return emojis_from_replier
+    
+    result = [{
+        'user': reply['user'],
+        'reactions': extract_reactions_from_reply(reply)
+        } for reply in replies]
+    return result
+
+pprint(get_replies_from_ts(client, 'C04SJDDSYHG', '1679727644.463259'))
