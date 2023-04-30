8fe24bc312b6d6bf10ba897fbad53492008cc272
lumiamitie <lumiamitie@gmail.com>
2023-04-30 13:22:14 +0900

    fix: post_group column 추가

--- src/feedback_check.py
+++ src/feedback_check.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 
-def run_feedback_checker(slack_token: str, channel_id: str, thread_ts: str):
+def run_feedback_checker(slack_token: str, channel_id: str, thread_ts: str, post_group: int):
     current_time = (datetime.today()).strftime('%Y-%m-%d %H:%M:%S')
     
     client = get_slack_client(slack_token)
     replies = get_replies_from_ts(client, channel_id, thread_ts)
     
     feedbacks = [
-        [r['user'], r['time'], current_time]
+        [str(post_group), r['user'], r['time'], current_time]
         for r in replies 
         if 'feedback' in r['reactions']
     ]
@@ -34,5 +34,6 @@ if __name__ == '__main__':
     SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
     CHANNEL_ID = 'C04SJDDSYHG'
     THREAD_TS = '1680698243.235909'
+    POST_GROUP = 1 # 업로드 주차마다 1씩 올려야 함
 
-    run_feedback_checker(SLACK_BOT_TOKEN, CHANNEL_ID, THREAD_TS)
+    run_feedback_checker(SLACK_BOT_TOKEN, CHANNEL_ID, THREAD_TS, POST_GROUP)
--- src/post.py
+++ src/post.py
@@ -7,13 +7,13 @@ from pprint import pprint
 from datetime import datetime
 
 
-def upload_posts(due_start_date: str, due_end_date: str):
+def upload_posts(post_group: int, due_start_date: str, due_end_date: str):
     current_time = (datetime.today()).strftime('%Y-%m-%d %H:%M:%S')
     google_svc_account = get_gspread_service_account()
     
     articles = get_contentful_articles()
     articles = filter_articles_by_duedate(articles, due_start_date, due_end_date)
-    articles = [[a['author_id'], a['title'], a['slug'], a['date'], current_time] for a in articles]
+    articles = [[str(post_group), a['author_id'], a['title'], a['slug'], a['date'], current_time] for a in articles]
 
     for article in articles:
         insert_worksheet(google_svc_account, 'PAP 시즌 2 퍼블리셔 제출 현황', 'season3_posts', article)
@@ -23,4 +23,5 @@ if __name__ == '__main__':
     # TODO : CLI에서 직접 실행할 경우 커맨드라인으로 명령어 받아서 커스텀으로 돌릴 수 있게 하기
     env_path = Path('.') / '.env'
     load_dotenv(dotenv_path=env_path)
-    upload_posts('2023-03-20', '2023-04-06')
+    POST_GROUP = 1 # 업로드 주차마다 1씩 올려야 함
+    upload_posts(POST_GROUP, '2023-03-20', '2023-04-06')
