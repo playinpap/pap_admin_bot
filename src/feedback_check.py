82f9e0e47e74e29ee6b1073ca04e5d1a8a82cdf2
lumiamitie <lumiamitie@gmail.com>
2023-05-12 00:05:10 +0900

    fix: 2주차 스크립트 파라미터로 수정

--- src/feedback_check.py
+++ src/feedback_check.py
@@ -26,12 +26,16 @@ def run_feedback_checker(slack_token: str, channel_id: str, thread_ts: str, post
 
 
 if __name__ == '__main__':
+    # poetry run python src/feedback_check.py
     env_path = Path('.') / '.env'
     load_dotenv(dotenv_path=env_path)
+
+    # POST_GROUP=1  https://playinpap.slack.com/archives/C04SJDDSYHG/p1680698243235909  =>  1680698243.235909
+    # POST_GROUP=2  https://playinpap.slack.com/archives/C04SJDDSYHG/p1683156600730909  =>  1683156600.730909
     
     SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
     CHANNEL_ID = 'C04SJDDSYHG'
-    THREAD_TS = '1680698243.235909'
-    POST_GROUP = 1 # 업로드 주차마다 1씩 올려야 함
+    THREAD_TS = '1683156600.730909'
+    POST_GROUP = 2 # 업로드 주차마다 1씩 올려야 함
 
     run_feedback_checker(SLACK_BOT_TOKEN, CHANNEL_ID, THREAD_TS, POST_GROUP)
